7b9548e95c966f3a09177cbd6a398f155c5173f4
christopergyman <christophergayiuman@gmail.com>
2024-01-15 20:25:52 +0000

    basic div sorted out

---
+++ chrisgyman-dev/src/app/components/project-bubble.tsx
@@ -0,0 +1,12 @@
+function ProjectBubble(){
+    return(
+        <div className="outline outline-[3px] outline-[#BDBDBD] m-5 max-w-[380px] rounded-lg h-[80px]">
+            <div className="outline-dotted inline-block m-2 w-[363px] h-[64px]">
+                <p>test</p> 
+            </div>
+            
+        </div>
+    );
+}
+
+export default ProjectBubble
--- chrisgyman-dev/tailwind.config.ts
+++ chrisgyman-dev/tailwind.config.ts
@@ -7,6 +7,11 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      custombgcolor: '#161A1D'
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
