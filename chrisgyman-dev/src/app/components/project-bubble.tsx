869ee857ff060375980135b6e97f14556a270397
christopergyman <christophergayiuman@gmail.com>
2024-01-15 20:56:30 +0000

    project bubble work in prog

--- chrisgyman-dev/src/app/components/project-bubble.tsx
+++ chrisgyman-dev/src/app/components/project-bubble.tsx
@@ -1,12 +1,39 @@
-function ProjectBubble(){
-    return(
-        <div className="outline outline-[3px] outline-[#BDBDBD] m-5 max-w-[380px] rounded-lg h-[80px]">
-            <div className="outline-dotted inline-block m-2 w-[363px] h-[64px]">
-                <p>test</p> 
-            </div>
-            
+import { link } from "fs";
+
+function ProjectBubble() {
+  const projectDetails = {
+    emoji: "📘",
+    title: "Haskell_compiler",
+    posttype: "Compiler",
+    description: "Compiler built from scratch",
+    language: "Haskell",
+    color: "Blue",
+    link: "https://github.com/christophergyman/haskell_compiler",
+  };
+
+  return (
+    <div className="outline outline-[3px] outline-[#BDBDBD] m-5 pt-2 max-w-[380px] rounded-lg h-[95px]">
+      <div className="inline-block m-2 w-[363px] h-[80px]">
+        <div className="flex flex-row">
+          <p>{projectDetails.emoji}</p>
+          <a href="https://github.com/christophergyman/haskell_compiler">
+            <p className="font-bold pl-2">{projectDetails.title}</p>
+          </a>
+          <div className="flex outline outline-[1px] outline-[#BDBDBD] ml-[12px] rounded-lg p-1 text-xs">
+            <p className="mx-2 text-[#BDBDBD]">{projectDetails.posttype}</p>
+          </div>
         </div>
-    );
+
+        <div className="text-[#BDBDBD]">
+          <p className="text-sm">{projectDetails.description}</p>
+          <div className="flex flex-row text-xs">
+            <p>{projectDetails.language}</p>
+            <p>{projectDetails.color}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ProjectBubble
+export default ProjectBubble;
--- chrisgyman-dev/src/app/page.tsx
+++ chrisgyman-dev/src/app/page.tsx
@@ -1,15 +1,17 @@
-import Image from 'next/image'
-import ProjectBubble from './components/project-bubble'
+import Image from "next/image";
+import ProjectBubble from "./components/project-bubble";
 
 export default function Home() {
   return (
     <main>
-      <div className='flex justify-center w-screen h-screen mt-[50px]'>
-        <div className='outline w-[85vh]'>
-          <h1 className='font-black text-white text-4xl text-center'>Chrisgyman.dev</h1>
-          <ProjectBubble/>
+      <div className="flex justify-center w-screen h-screen mt-[50px]">
+        <div className="outline w-[85vh]">
+          <h1 className="font-black text-white text-4xl text-center">
+            Chrisgyman.dev
+          </h1>
+          <ProjectBubble />
         </div>
       </div>
     </main>
-  )
+  );
 }
