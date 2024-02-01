3d14a7707e92bf5a5de66044b3e3d84bfd2c0990
christophergyman <christophergayiuman@gmail.com>
2024-02-01 16:41:10 +0000

    moved to public

--- chrisgyman-dev/src/app/post/[postName]/page.tsx
+++ chrisgyman-dev/src/app/post/[postName]/page.tsx
@@ -10,7 +10,7 @@ export default async function FullPost({
   };
 }) {
   //markdown filepaht
-  const markdownFilesPath: string = "/src/app/markdownfiles/";
+  const markdownFilesPath: string = "/public/markdownfiles/";
   const finalPath: string = (await markdownFilesPath) + postName + ".md";
 
   return (
